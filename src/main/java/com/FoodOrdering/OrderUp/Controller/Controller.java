b03dab5e8f0dfbb6161c1151c7c25a8bdc364703
ddaatts1 <dodat743@gmail.com>
2023-02-20 14:21:54 +0700

    first commit

---
+++ src/main/java/com/FoodOrdering/OrderUp/Controller/Controller.java
@@ -0,0 +1,20 @@
+package com.FoodOrdering.OrderUp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    @GetMapping("/index")
+    public String index(){
+        return "Do Tien Dat";
+    }
+
+    @GetMapping("/index2")
+    public String index2(){
+        return "index2";
+    }
+
+}
